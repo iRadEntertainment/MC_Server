7083ba6ad2d5bc73cb2c6b18e3a8374d15c3cee4
iRadEntertainment <devitadario@gmail.com>
2019-11-24 02:29:48 +0100

    Client->Server script updates in Control Room

--- remote_func.gd
+++ remote_func.gd
@@ -26,9 +26,45 @@ remote func send_last_server_entry(entry,id = null):
 		rpc("receive_last_server_entry",entry)
 
 func ask_user_name(id):
-	var user_name = null
-	user_name = rpc_id(id,"return_user_name")
+	var user_name = rpc_id(id,"return_user_name")
 	return user_name
+
+#================ FILE MANAGEMENT ======================
+remote func send_file_content(which):
+	var sender_id = multiplayer.get_rpc_sender_id()
+	server.log_print( str( sender_id ) + " requested file %s"%which )
+	
+	var allowed = ["remote_func","server"]
+	if not which in allowed:
+		return false
+	var path
+	match which:
+		"remote_func": path = "res://remote_func.gd"
+		"server":      path = "res://server.gd"
+	
+	var file = File.new()
+	file.open(path, File.READ)
+	var content = file.get_as_text()
+	file.close()
+	rpc_id(sender_id,"receive_file_content",content)
+
+remote func override_file(which, content):
+	var sender_id = multiplayer.get_rpc_sender_id()
+	server.log_print( str( sender_id ) + " overriding file %s"%which )
+	
+	var allowed = ["remote_func","server"]
+	if not which in allowed:
+		return false
+	var path
+	match which:
+		"remote_func": path = "res://remote_func.gd"
+		"server":      path = "res://server.gd"
+	
+	var file = File.new()
+	file.open(path, File.WRITE)
+	file.store_string(content)
+	file.close()
+
 
 #================ TESTS ======================
 remote func test():
